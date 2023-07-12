35167a1b42cef3143650e2175d93d3484aaeea2f
Vidyasagar Singadi <vidyasagar.singadi@gmail.com>
2023-07-13 00:28:31 +0530

    created manual problem list container

---
+++ frontend/src/problem.js
@@ -0,0 +1,25 @@
+import './problem.css';
+
+function Problem() {
+  return (
+    <>
+    <div className="main-wrapper">
+      <div className="main-container">
+        <h1>Problems</h1>
+        <div className="problems-list">
+          <div className="problem-container">
+            <div className="problem-id">
+              1.
+            </div>
+            <div className="problem-title">
+              Two Sum
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Problem;
