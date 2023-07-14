8e27b303e7f411b68ec012635fe5eafe72653ca9
Vidyasagar Singadi <vidyasagar.singadi@gmail.com>
2023-07-14 17:47:41 +0530

    simple boxes model for problem display and other components

--- frontend/src/pages/P1.js
+++ frontend/src/pages/P1.js
@@ -1,4 +1,4 @@
-export function P1(){
+export function P1() {
     return (
         <>
             <div className="problem-page-wrapper">
@@ -13,33 +13,41 @@ export function P1(){
                             </div>
                         </div>
                     </div>
-                    <div className="code-box-wrapper">
-                        <div className="code-box">
-
+                    <div className="problem-simulation-container">
+                        <div className="code-box-wrapper">
+                            <div className="code-box">
+                                <h4>Code</h4>
+                                <textarea className="code-box-textarea" placeholder="Write your code here..."/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="input-box-wrapper">
-                        <div className="input-box">
-
+                        <div className="code-simulation-container">
+                            <div className="boxes-wrapper">
+                                <div className="input-box-wrapper">
+                                    <div className="input-box">
+                                        <h5>Input</h5>
+                                        <textarea className="code-box-textarea" placeholder="Write your input here..."/>
+                                    </div>
+                                </div>
+                                <div className="output-box-wrapper">
+                                    <div className="output-box">
+                                        <h5>Output</h5>
+                                        <textarea className="code-box-textarea" placeholder="Wait for the output..."/>
+                                    </div>
+                                </div>
+                                <div className="verdict-box-wrapper">
+                                    <div className="verdict-box">
+                                        <h5>Verdict</h5>
+                                        <textarea className="code-box-textarea" placeholder="Wait for the verdict..."/>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="buttons-wrapper">
+                                    <button className="run-button">Run</button>
+                                    <button className="submit-button">Submit</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="output-box-wrapper">
-                        <div className="output-box">
 
-                        </div>
-                    </div>
-                    <div className="verdict-box-wrapper">
-                        <div className="verdict-box">
-
-                        </div>
-                    </div>
-                    <div className="buttons-wrapper">
-                        <div className="run-button">
-
-                        </div>
-                        <div className="submit-button">
-
-                        </div>
                     </div>
                 </div>
             </div>
