7d49af2a616b980b8881b4ad43d93ee781a579c9
Vidyasagar Singadi <vidyasagar.singadi@gmail.com>
2023-07-15 22:05:30 +0530

    imported libs for backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
-import express from 'express'
+import express from 'express';
+import router from './routes/routes.js';
+import DBConnection from './database/db.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('This is backend :)');
-})
+// app.get('/', (req, res) => {
+//     res.send('This is backend :)');
+// })
+
+app.use('/', router);
+
+DBConnection();
 
 app.listen(8000, ()=>{
     console.log("Server is running on port 8000!");
-})
+});
